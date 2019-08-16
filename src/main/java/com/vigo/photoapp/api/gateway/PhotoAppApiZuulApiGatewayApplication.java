636c50afa331ba8ac673d60081261e05b6ea33e8
Vigo92 <obiaugochukwu@gmail.com>
2019-08-16 19:01:55 +0100

    Api Gateway

---
+++ src/main/java/com/vigo/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.vigo.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class PhotoAppApiZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZuulApiGatewayApplication.class, args);
+	}
+
+}
